c5400c19a53f5b631d4fdf291881d980f14c5f43
carvaq <shoesforbreakfast@gmail.com>
2018-05-27 20:38:04 +0200

    Converted test classes to kotlin

---
+++ app/src/test/java/com/cvv/fanstaticapps/randomticker/helper/TimeSeekBarChangeListenerTest.kt
@@ -0,0 +1,59 @@
+package com.cvv.fanstaticapps.randomticker.helper
+
+import android.widget.SeekBar
+import android.widget.TextView
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnit
+import org.robolectric.RobolectricTestRunner
+
+/**
+ * Created by Carla
+ * Date: 05/02/2018
+ * Project: RandomTicker
+ */
+
+@RunWith(RobolectricTestRunner::class)
+class TimeSeekBarChangeListenerTest {
+
+    @get:Rule
+    var rule = MockitoJUnit.rule()
+
+    @Mock
+    private val seekBarMinutes: SeekBar = mock()
+    @Mock
+    private val seekBarSeconds: SeekBar = mock()
+    @Mock
+    private val displayTextView: TextView = mock()
+
+    private var timeSeekBarChangeListener: TimeSeekBarChangeListener? = null
+
+    @Before
+    fun setUp() {
+        timeSeekBarChangeListener = TimeSeekBarChangeListener(displayTextView, seekBarMinutes, seekBarSeconds)
+    }
+
+    @Test
+    fun testCorrectFormat() {
+        setSeekBarValues(12, 2)
+        verify<TextView>(displayTextView).text = "12m 02s"
+
+        setSeekBarValues(2, 2)
+        verify<TextView>(displayTextView).text = "02m 02s"
+
+        setSeekBarValues(12, 12)
+        verify<TextView>(displayTextView).text = "12m 12s"
+    }
+
+    private fun setSeekBarValues(minutes: Int, seconds: Int) {
+        whenever(seekBarMinutes.progress).thenReturn(minutes)
+        whenever(seekBarSeconds.progress).thenReturn(seconds)
+        timeSeekBarChangeListener!!.onProgressChanged(null, 0, true)
+    }
+}
