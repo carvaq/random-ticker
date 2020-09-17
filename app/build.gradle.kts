0ae2b2c1504c6c4fe6956e3ed9edb766e5b1243d
carla <carla@getbring.com>
2020-09-17 21:19:27 +0200

    [Gradle] update to kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,17 +3,16 @@ plugins {
     id("kotlin-android")
     id("kotlin-kapt")
     id("kotlin-android-extensions")
-    id("com.github.triplet.play") version (Versions.triplet)
+    id("com.github.triplet.play")
     id("com.google.android.gms.oss-licenses-plugin")
     id("com.google.firebase.crashlytics")
     id("dagger.hilt.android.plugin")
     id("com.google.gms.google-services")
 }
 
-
 val major = 1
-val minor = 6
-val patch = 5
+val minor = 1
+val patch = 0
 
 val generatedVersionName = String.format("%s%02d%02d", major, minor, patch)
 val generateVersionCode = Integer.parseInt(generatedVersionName)
@@ -32,21 +31,17 @@ android {
         versionCode = generateVersionCode
         versionName = generatedVersionName
 
-        testInstrumentationRunner = "com.fanstaticapps.randomticker.TickerTestRunner"
-        setTestInstrumentationRunnerArguments(mutableMapOf("clearPackageData" to "true"))
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     signingConfigs {
         create("release") {
-            val fanstaticKeyAlias = System.getenv("RELEASE_KEY_ALIAS")
-            println("Adding release config for production $fanstaticKeyAlias")
-
-            if (fanstaticKeyAlias != null) {
+            if (project.hasProperty("fanstaticKeyalias")) {
                 println("Adding release config for production")
                 keyAlias = fanstaticKeyAlias
-                keyPassword = System.getenv("RELEASE_KEY_PASSWORD")
-                storeFile = file("$rootDir/fanstaticapps.keystore")
-                storePassword = System.getenv("RELEASE_KEYSTORE_PASSWORD")
+                keyPassword = fanstaticKeyPassword
+                storeFile = file(fanstaticFile)
+                storePassword = fanstaticStorePassword
             }
         }
     }
@@ -64,89 +59,54 @@ android {
         unitTests.apply {
             isReturnDefaultValues = true
         }
-        execution = "ANDROIDX_TEST_ORCHESTRATOR"
-        animationsDisabled = true
     }
-    sourceSets {
-        getByName("androidTest").assets.srcDirs("$projectDir/schemas")
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
 }
 
 dependencies {
     implementation(Kotlin.stdlib)
-
     implementation(AndroidLibs.material)
     implementation(AndroidLibs.cardview)
     implementation(AndroidLibs.appcompat)
     implementation(AndroidLibs.constraint_layout)
     implementation(AndroidLibs.room_runtime)
-    implementation(AndroidLibs.room_ktx)
-
+    implementation(AndroidLibs.room_rxjava2)
     implementation(AndroidLibs.livedata)
     implementation(AndroidLibs.viewmodel)
     implementation(AndroidLibs.extensions)
     implementation(AndroidLibs.preference)
-    implementation(AndroidLibs.activity)
-    implementation(AndroidLibs.fragment)
-    implementation(AndroidLibs.coroutines)
 
     implementation(Firebase.crashlytics)
 
+    implementation(Libs.rxAndroid)
     implementation(Libs.hilt)
-    implementation(Libs.hilt_viewmodel)
     implementation(Libs.timber)
     implementation(Libs.lottie)
+    implementation(Libs.pulsator)
     implementation(Libs.ultimatePicker)
 
     implementation(Gms.oss)
 
     kapt(AndroidLibs.room_compiler)
-    kapt(Libs.hilt_compiler)
-    kapt(Libs.hilt_viewmodel_compiler)
+    kapt(Libs.hilt_compile)
 
     testImplementation(TestLibs.mockito_core)
     testImplementation(TestLibs.junit)
-    testImplementation(TestLibs.test_core)
     testImplementation(TestLibs.robolectric)
     testImplementation(TestLibs.mockito_kotlin)
-    testImplementation(TestLibs.assertions_junit)
-    testImplementation(TestLibs.hilt_testing)
-    testImplementation(TestLibs.coroutines_testing)
-    testImplementation(TestLibs.assertions_truth)
-    testImplementation(TestLibs.assertions_google_truth)
-    kaptTest(Libs.hilt_compiler)
 
-    androidTestImplementation(TestLibs.room_testing)
-    androidTestImplementation(TestLibs.uiautomator)
-    androidTestImplementation(TestLibs.test_core)
-    androidTestImplementation(TestLibs.rules)
-    androidTestImplementation(TestLibs.junit_runner)
-    androidTestImplementation(TestLibs.espresso_core)
-    androidTestImplementation(TestLibs.espresso_idling)
-    androidTestImplementation(TestLibs.espresso_contrib)
-    androidTestImplementation(TestLibs.assertions_junit)
-    androidTestImplementation(TestLibs.assertions_truth)
-    androidTestImplementation(TestLibs.assertions_google_truth)
-    androidTestImplementation(TestLibs.hilt_testing)
-    kaptAndroidTest(Libs.hilt_compiler)
-    androidTestUtil(TestLibs.orchestrator)
+    androidTestImplementation(AndroidLibs.uiautomator)
+    androidTestImplementation(AndroidLibs.rules)
+    androidTestImplementation(AndroidLibs.espresso_core)
 }
 
 play {
-    println("Configuring play plugin ${file("$rootDir/play_store_secret.json").exists()}")
     if (file("$rootDir/play_store_secret.json").exists()) {
-        defaultToAppBundles.set(true)
-        serviceAccountCredentials.set(file("$rootDir/play_store_secret.json"))
-    } else {
-        enabled.set(false)
+        defaultToAppBundles = true
+        serviceAccountCredentials = file("$rootDir/play_store_secret.json")
     }
 }
 
@@ -159,3 +119,10 @@ kapt {
         arg("room.expandProjection", "true")
     }
 }
+
+val fanstaticKeyAlias: String by project
+val fanstaticKeyPassword: String by project
+val fanstaticFile: String by project
+val fanstaticStorePassword: String by project
+
+
