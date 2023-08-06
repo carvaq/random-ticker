6d3da7c999c03c7fb2d21873a91772e3b9573ba4
carvaq <noreply@carvaq-github.com>
2023-08-06 10:58:57 +0200

    update to 8.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0-rc01")
+        classpath("com.android.tools.build:gradle:8.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
