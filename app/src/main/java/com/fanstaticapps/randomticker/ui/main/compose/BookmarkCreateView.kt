c0e0fa6f75a2abdd5a5531bdfbca011adcfacefc
carvaq <noreply@carvaq-github.com>
2023-08-08 22:24:59 +0200

    adds other required fields

--- app/src/main/java/com/fanstaticapps/randomticker/ui/main/compose/BookmarkCreateView.kt
+++ app/src/main/java/com/fanstaticapps/randomticker/ui/main/compose/BookmarkCreateView.kt
@@ -3,6 +3,7 @@ package com.fanstaticapps.randomticker.ui.main.compose
 import android.widget.NumberPicker
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,7 +19,10 @@ import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -52,6 +56,10 @@ fun BookmarkCreateView(
     ) {
         val minBoundary = remember { mutableStateOf(bookmark.min) }
         val maxBoundary = remember { mutableStateOf(bookmark.max) }
+        val name = remember { mutableStateOf(bookmark.name) }
+        val autoRepeat = remember { mutableStateOf(bookmark.autoRepeat) }
+        BookmarkName(name.value) { name.value = it }
+        Spacer(Modifier.height(16.dp))
         Column {
             BoundaryView(
                 contentBindingModifier = Modifier
@@ -60,7 +68,7 @@ fun BookmarkCreateView(
                 boundary = minBoundary,
                 boundaryType = R.string.from
             )
-            Spacer(Modifier.height(24.dp))
+            Spacer(Modifier.height(16.dp))
             BoundaryView(
                 contentBindingModifier = Modifier
                     .heightIn(max = 240.dp)
@@ -69,17 +77,10 @@ fun BookmarkCreateView(
                 boundaryType = R.string.to
             )
         }
-        Spacer(
-            modifier = Modifier.height(36.dp)
-        )
-        Button(modifier = Modifier.fillMaxWidth(),
-            onClick = { save(bookmark.updateBoundaries(minBoundary.value, maxBoundary.value)) }) {
-            Icon(imageVector = Icons.Default.Save, contentDescription = null)
-            Text(
-                modifier = Modifier.padding(start = 16.dp),
-                text = stringResource(id = R.string.save_button)
-            )
-        }
+        Spacer(modifier = Modifier.height(16.dp))
+        AutoRepeat(autoRepeat.value) { autoRepeat.value = it }
+        Spacer(modifier = Modifier.height(36.dp))
+        SaveButton(minBoundary, maxBoundary, save, bookmark, name, autoRepeat)
         Spacer(Modifier.height(16.dp))
         Button(onClick = { delete(bookmark) }, colors = ButtonDefaults.textButtonColors()) {
             Icon(imageVector = Icons.Default.Delete, contentDescription = null)
@@ -90,6 +91,55 @@ fun BookmarkCreateView(
         }
 
     }
+}
+
+@Composable
+private fun SaveButton(
+    minBoundary: MutableState<Boundary>,
+    maxBoundary: MutableState<Boundary>,
+    save: (Bookmark) -> Unit,
+    bookmark: Bookmark,
+    name: MutableState<String>,
+    autoRepeat: MutableState<Boolean>
+) {
+    Button(modifier = Modifier.fillMaxWidth(),
+        enabled = minBoundary.value < maxBoundary.value,
+        onClick = {
+            save(
+                bookmark.updateBoundaries(minBoundary.value, maxBoundary.value)
+                    .copy(name = name.value, autoRepeat = autoRepeat.value)
+            )
+        }) {
+        Icon(imageVector = Icons.Default.Save, contentDescription = null)
+        Text(
+            modifier = Modifier.padding(start = 16.dp),
+            text = stringResource(id = R.string.save_button)
+        )
+    }
+}
+
+@Composable
+fun AutoRepeat(value: Boolean, onCheckedChanged: (Boolean) -> Unit) {
+    Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        Checkbox(checked = value, onCheckedChange = onCheckedChanged)
+        Text(text = stringResource(id = R.string.auto_repeat))
+    }
+}
+
+@Composable
+fun BookmarkName(bookmarkName: String, updateName: (String) -> Unit) {
+    OutlinedTextField(
+        value = bookmarkName,
+        onValueChange = updateName,
+        label = {
+            Text(
+                text = stringResource(id = R.string.bookmark_hint),
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        },
+        modifier = Modifier.fillMaxWidth(),
+        textStyle = MaterialTheme.typography.bodyMedium,
+    )
 }
 
 
@@ -159,7 +209,7 @@ private fun NumberPicker.initialize(
     }
 }
 
-@Preview(heightDp = 635, showBackground = true)
+@Preview(heightDp = 2000, showBackground = true)
 @Composable
 fun BookmarkCreatePreview() {
     BookmarkCreateView(bookmark = Bookmark(maximumSeconds = 12, maximumHours = 1),
