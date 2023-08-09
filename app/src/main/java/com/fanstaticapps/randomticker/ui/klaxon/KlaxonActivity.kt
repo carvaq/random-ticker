c38bbeeef75f77b0a64245da2eebb2127f48b9c8
carvaq <carvaq@githbub.con>
2023-08-09 23:05:14 +0200

    refactor klaxon view to be more modern

--- app/src/main/java/com/fanstaticapps/randomticker/ui/klaxon/KlaxonActivity.kt
+++ app/src/main/java/com/fanstaticapps/randomticker/ui/klaxon/KlaxonActivity.kt
@@ -1,44 +1,35 @@
 package com.fanstaticapps.randomticker.ui.klaxon
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Stop
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.fanstaticapps.randomticker.R
@@ -62,13 +53,14 @@ class KlaxonActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val bookmark = klaxonViewModel.currentBookmark.observeAsState().value
-            if (bookmark != null) {
-                val windowSizeClass = calculateWindowSizeClass(this)
-                KlaxonView(
-                    bookmark,
-                    windowSizeClass.heightSizeClass == WindowHeightSizeClass.Compact && windowSizeClass.widthSizeClass == WindowWidthSizeClass.Compact
-                )
+            AppTheme {
+                val bookmark = klaxonViewModel.currentBookmark.observeAsState().value
+                if (bookmark != null) {
+                    val windowSizeClass = calculateWindowSizeClass(this)
+                    KlaxonView(
+                        bookmark
+                    )
+                }
             }
         }
         klaxonViewModel.currentBookmark.observe(this) {
@@ -83,80 +75,75 @@ class KlaxonActivity : BaseActivity() {
     }
 
     @Composable
-    private fun KlaxonView(bookmark: Bookmark, compactSize: Boolean) {
-        AppTheme {
-            Column(
+    private fun KlaxonView(bookmark: Bookmark) {
+        val colorList = listOf(
+            MaterialTheme.colorScheme.primary,
+            MaterialTheme.colorScheme.primaryContainer
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Brush.verticalGradient(colorList))
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Spacer(modifier = Modifier.weight(0.3f))
+            Text(
+                text = bookmark.name,
+                style = MaterialTheme.typography.displayLarge,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Row(
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .background(MaterialTheme.colorScheme.background)
-                    .fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly
+                    .fillMaxHeight(0.15f)
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.Center
             ) {
-                Text(
-                    text = bookmark.name, style = MaterialTheme.typography.headlineLarge
-                )
-                val size = if (compactSize) 80.dp else 128.dp
-                Box(
-                    modifier = Modifier.defaultMinSize(size, size),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Pulsating(size = size)
-                    IconButton(onClick = {
+                val buttonModifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                Button(
+                    modifier = buttonModifier,
+                    onClick = {
                         klaxonViewModel.cancelTimer(this@KlaxonActivity)
                         openMainActivity(bookmark)
-                    }) {
-                        Icon(
-                            modifier = Modifier.size(size),
-                            imageVector = Icons.Filled.Close,
-                            tint = MaterialTheme.colorScheme.onPrimaryContainer,
-                            contentDescription = stringResource(id = android.R.string.cancel)
+                    },
+                    shape = ButtonDefaults.outlinedShape
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Stop,
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                        contentDescription = stringResource(id = R.string.action_stop)
+                    )
+                }
+                if (!bookmark.autoRepeat) {
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Button(
+                        onClick = { autoRepeatTicker(bookmark) },
+                        modifier = buttonModifier,
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.action_repeat)
                         )
                     }
-                }
-                Button(
-                    onClick = { autoRepeatTicker(bookmark) },
-                    modifier = Modifier.alpha(if (bookmark.autoRepeat) 0f else 1f)
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.action_repeat)
-                    )
                 }
             }
         }
     }
 
-    @Composable
-    fun Pulsating(size: Dp) {
-        val infiniteTransition = rememberInfiniteTransition(label = "Pulsating")
-
-        val scale by infiniteTransition.animateFloat(
-            initialValue = 1f,
-            targetValue = 2.5f,
-            animationSpec = infiniteRepeatable(
-                animation = tween(1800),
-                repeatMode = RepeatMode.Reverse
-            ),
-            label = "ScalePulsating"
-        )
-
-        Box(modifier = Modifier.scale(scale)) {
-            Surface(
-                color = MaterialTheme.colorScheme.primaryContainer,
-                shape = CircleShape,
-                modifier = Modifier.size(size),
-                content = {}
-            )
-        }
-    }
-
-    @Preview(device = Devices.PIXEL_4)
+    @Preview(uiMode = UI_MODE_NIGHT_NO)
+    @Preview(uiMode = UI_MODE_NIGHT_YES)
     @Composable
     private fun KlaxonPreview() {
-        KlaxonView(
-            Bookmark("Test"),
-            true
-        )
+        AppTheme {
+            KlaxonView(
+                Bookmark("Test")
+            )
+        }
     }
 
     private fun autoRepeatTicker(bookmark: Bookmark) {
